131d31516e450e56beb75fe34f57337d8288813c
Mediwynn M Maart <mediwynnm@gmail.com>
2024-09-24 23:18:21 +0200

    Added passwords

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('posts/', include('posts.urls')),
-    path('followers/', include('followers.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/posts/', include('posts.urls')),
+    path('api/followers/', include('followers.urls')), 
 ]
 
--- social_media_api/users/serializers.py
+++ social_media_api/users/serializers.py
@@ -1,16 +1,24 @@
+# users/serializers.py
 from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    # Make password write-only
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        # Create user using create_user to ensure password is hashed
+        user = User(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            bio=validated_data.get('bio', ''),  # Use get to provide a default value
+            profile_picture=validated_data.get('profile_picture', None)  # Handle optional fields
         )
+        user.set_password(validated_data['password'])  # Hash the password
+        user.save()  # Save the user instance to the database
         return user
